3b07fad534e92958524a4b6ed13e91d84fe91c21
Nat Eguchi <nat@universlabs.co.uk>
2022-02-17 13:57:28 +0000

    Actually commit my changes lol

--- cmd/gateway/gcs/gateway-gcs.go
+++ cmd/gateway/gcs/gateway-gcs.go
@@ -312,7 +312,7 @@ func gcsToObjectError(err error, params ...string) error {
 			err = minio.BucketAlreadyOwnedByYou{Bucket: bucket}
 			break
 		}
-		if message == "Sorry, that name is not available. Please try a different one." {
+		if regexp.MustCompile("A Cloud Storage bucket named.*already exists").MatchString(message) || message == "The requested bucket name is not available. The bucket namespace is shared by all users of the system. Please select a different name and try again." || message == "Sorry, that name is not available. Please try a different one." {
 			err = minio.BucketAlreadyExists{Bucket: bucket}
 			break
 		}
